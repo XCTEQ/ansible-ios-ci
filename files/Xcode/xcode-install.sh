5c217065494cfe8d92b6bdabf5166f2087317ccd
Shashikant86 <shashikant.jagtap@icloud.com>
2017-02-18 13:05:15 +0000

    Added Swift Setup for the playbook

--- files/Xcode/xcode-install.sh
+++
@@ -1,5 +0,0 @@
-echo pwd
-echo "=========Installing Xcode from xip file.. It will take some time========== "
-open -FWga "Archive Utility" --args ~/Documents/Xcode*.xip
-echo "==Successfully Extracted Xcode, Moving to Applications===="
-mv ~/Documents/Xcode*.app ~/Applications/
